b13cc7d8488f469ce9509d8894413efe4d1ea41d
elishayan <37676644+elishayan@users.noreply.github.com>
2023-05-29 15:48:49 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,8 @@
 import dash
-import dash_html_components as html
 import plotly.graph_objects as go
 from plotly.subplots import make_subplots
 import dash_daq as daq
 #from jupyter_dash import JupyterDash
-import dash
 import dash_bootstrap_components as dbc
 import dash_html_components as html
 import dash_core_components as dcc
